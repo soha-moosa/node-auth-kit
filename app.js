6f42993713029594bdc045b59e00f3b03fd03cf6
soha-moosa <soha.moosa@yahoo.com>
2019-09-04 18:36:55 +0500

    [feat] - connection to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const app = require('express')();
+const mongoose = require('mongoose');
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running at PORT ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log(`Connected Successfully to MongoDB!`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.log(`Connection Failed to MongoDB : ${err}`));
